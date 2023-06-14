477cb60ac7e987f3392527ba1677a8c634906620
JemimahLobo <jemimahl189@gmail.com>
2023-06-14 11:26:48 -0500

    Resolved merge conflicts

--- jemimah-counter-app/app/components/counter.tsx
+++ jemimah-counter-app/app/components/counter.tsx
@@ -6,12 +6,8 @@ const counter = () => {
     const IncremementCounter = () => {
         setCount(count + 1);
     };
-<<<<<<< HEAD
     
     const DecrementCounter = () => {
-=======
-     const DecrementCounter = () => {
->>>>>>> e269ecd8d2145cdc2f1f229e02258d482dcff95e
         setCount(count - 1);
         
     };
@@ -25,15 +21,9 @@ const counter = () => {
         <div>
             <h1 className="componentHeading"> Counter </h1>
             <p className={count > 10 ? "greater-than-ten": ""}  > count Count: {count} </p>
-<<<<<<< HEAD
             <button className="fancyButton" onClick={() => IncremementCounter()}> Increment </button>
             <button className="fancyButton" onClick={() => DecrementCounter()}>Decrement </button>
             <button className="fancyButton" onClick={() => ResetCounter()}>Reset </button>
-=======
-            <button className="fancyButton" onClick={() => incremementCounter}> Increment </button>
-            <button className="fancyButton"> Decrement </button>
-            <button className="fancyButton"> Reset </button>
->>>>>>> e269ecd8d2145cdc2f1f229e02258d482dcff95e
 
         </div>
     );
