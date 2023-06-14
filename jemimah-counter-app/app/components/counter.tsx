d6dbb1fb1fd385ba6b11cee2e65da433cd54789e
JemimahLobo <jemimahl189@gmail.com>
2023-06-14 11:23:09 -0500

    syncing with github

--- jemimah-counter-app/app/components/counter.tsx
+++ jemimah-counter-app/app/components/counter.tsx
@@ -2,22 +2,31 @@ import { useState } from "react"
 
 
 const counter = () => {
-    const[count, setCount] = useState(0);
-
-    const incremementCounter = () => {
+    
+    const IncremementCounter = () => {
         setCount(count + 1);
-    }
+    };
+    
+    const DecrementCounter = () => {
+        setCount(count - 1);
+        
+    };
+    const ResetCounter = () => {
+        setCount(0)
+    };
 
+    const[count, setCount] = useState(0);
     return (
+        
         <div>
             <h1 className="componentHeading"> Counter </h1>
-            <p className="countLabel"> Count: {count} </p>
-            <button className="fancyButton" onClick={() => incremementCounter}> Increment </button>
-            <button className="fancyButton"> Decrement </button>
-            <button className="fancyButton"> Reset </button>
+            <p className={count > 10 ? "greater-than-ten": ""}  > count Count: {count} </p>
+            <button className="fancyButton" onClick={() => IncremementCounter()}> Increment </button>
+            <button className="fancyButton" onClick={() => DecrementCounter()}>Decrement </button>
+            <button className="fancyButton" onClick={() => ResetCounter()}>Reset </button>
 
         </div>
-    )
-}
+    );
+};
 
 export default counter;
